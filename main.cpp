dd9942a6787e23058719d3f1d38f8a97e898561d
SaadBerrada16 <116112545+SaadBerrada16@users.noreply.github.com>
2022-12-11 11:39:54 -0300

    Arduino

--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
-//=====[Libraries]=============================================================
-
-#include "smart_home_system.h"
-
-//=====[Main function, the program entry point after power on or reset]========
-
-int main()
+int digitalPin = 4;
+int analogPin = A0; // KY-026 analog interface
+int digitalVal; // digital readings
+int analogVal; //analog readings
+ 
+void setup()
 {
-    smartHomeSystemInit();
-    while (true) {
-        smartHomeSystemUpdate();
-    }
+pinMode(digitalPin, OUTPUT);
+digitalWrite(digitalPin, LOW);
+}
+ 
+void loop()
+{
+// lire la valeur retourné par le capteur de lumière
+analogVal = analogRead(analogPin);
+if (analogVal>670){ // lorsque la capteur lumière détecte une luminosité très basse
+digitalWrite(digitalPin, HIGH); // allumer LED rouge
+} else {
+digitalWrite(digitalPin, LOW); // éteindre LED rouge
+}
+delay(100);
 }
